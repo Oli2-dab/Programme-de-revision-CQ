58d9028a70e7e55e3fc27bf58be4022294fb6606
Oli2-dab <olivier@ipsodie.com>
2026-08-15 17:43:27 -0400

    0.0.4

--- Principale/Pages/acceuil.py
+++ Principale/Pages/acceuil.py
@@ -58,7 +58,7 @@ avertissement.write("Cette platforme est utiliser pour différent projet.")
 avertissement.write("Si vous renconter des erreurs, svp m'écrire pour que je puisse les corrigées pour que les autres ne les rencontres pas.")
 
 if st.session_state.jeulancé == False :
-    st.write("Version 0.0.3")
+    st.write("Version 0.0.4")
 
     st.markdown("""
                 -**0.0.0**  
