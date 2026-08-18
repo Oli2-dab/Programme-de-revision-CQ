41236b6793b2361be38e11a4375fecef58267a91
Oli2-dab <olivier@ipsodie.com>
2026-08-18 14:22:35 -0400

    1.0.29

--- Principale/acceuil.py
+++ Principale/acceuil.py
@@ -45,7 +45,7 @@ avertissement.write("Cette platforme est utiliser pour différent projet.")
 avertissement.write("Si vous renconter des erreurs, svp m'écrire pour que je puisse les corrigées pour que les autres ne les rencontres pas.")
 
 if st.session_state.etape == "choix_matière" :
-    st.write("Version 1.0.28")
+    st.write("Version 1.0.29")
 
     st.markdown("""
     -**1.0.28**  
